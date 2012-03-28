902778d5a678170b9b95c960464804ceeacd3b31
Bogdan Gusiev <agresso@gmail.com>
2012-03-28 14:06:44 +0300

    Rename classes

--- public/javascripts/skins/pointer.js
+++ public/javascripts/skins/pointer.js
@@ -1,7 +1,6 @@
 $(function () {
   $.errors.format = '<div class="error-message message"><span class="bg"></span><span class="text-message"></span><a href="#" class="close-notice"><span></span></a></div>'
   $.errors.messageClass = 'text-message'
-  $.errors.activationClass = 'field_with_errors'
   $('.ajax-form').ajaxForm();
   $('.close-notice').live("click", function(event) {
     event.preventDefault();
